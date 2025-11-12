97a1d8de6aedb2541e1ff8653cb9e3db54026eaf
Daniel Hoyos  Celdrán <dceldran@essigplm.com>
2025-11-12 18:53:20 +0100

    resta de dies que queden

--- src/app/components/daily-summary/daily-summary.ts
+++ src/app/components/daily-summary/daily-summary.ts
@@ -32,6 +32,9 @@ import { Observable } from 'rxjs';
                 <span class="text-2xl font-bold text-primary block">
                   {{ formatDuration(summary.totalDurationMs) }}
                 </span>
+                <span class="text-m text-secondary block">
+                  Queden: {{ formatDuration(28800000-summary.totalDurationMs) }}
+                </span>
               </div>
             </div>
           }
@@ -40,6 +43,7 @@ import { Observable } from 'rxjs';
     </div>
   `,
 })
+
 export class DailySummaryComponent {
   private timeEntryService = inject(TimeEntryService);
   
