2672c32939a14827b8454292e12442188bf4b7ed
Daniel Hoyos Celdrán <101184004+danielhoyosceldran@users.noreply.github.com>
2025-11-14 19:59:32 +0100

    margin top added

--- src/app/components/time-entry-list/time-entry-list.ts
+++ src/app/components/time-entry-list/time-entry-list.ts
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   template: `
-    <div class="p-6 bg-white shadow-xl rounded-lg border border-gray-100">
+    <div class="p-6 bg-white shadow-xl rounded-lg border border-gray-100 mt-8">
       <div class="flex justify-between items-center mb-4">
         <h2 class="text-2xl font-bold text-gray-800">Tracking History</h2>
         <button
